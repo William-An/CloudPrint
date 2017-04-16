c907bbb2b30427db6a8c71dc0a29f8312ecd0437
Weili An <China_Aisa@live.com>
2017-04-16 23:09:37 +0800

    Finish testing

--- server.py
+++ server.py
@@ -9,29 +9,23 @@ urls = ('/upload', 'Upload')
 class Upload:
     def GET(self):
         web.header("Content-Type","text/html; charset=utf-8")
-        return  """
-                    <html><head></head><body>
-					<form method="POST" enctype="multipart/form-data" action="">
-					<input type="file" name="myfile" />
-					<br/>
-					<br/>
-					<input type="submit" />
-					</form>
-					</body></html>
-                """
+        render = web.template.render('static/')
+        return render.index()
 
     def POST(self):
         x = web.input(myfile={})
-        filedir = '\\File'                                  # change this to the directory you want to store the file in.
+        filedir = '.\\File'                                  # change this to the directory you want to store the file in.
         if 'myfile' in x:                                   # to check if the file-object is created
             filepath=x.myfile.filename.replace('\\','/')    # replaces the windows-style slashes with linux ones.
             filename=filepath.split('\\')[-1]               # splits the and chooses the last part (the filename with extension)
-            if 'doc' not in filename[-1:-4]:                # Check if doc, TODO integrate in HTML
-                raise web.seeother('/upload')
             fout = open(filedir +'\\'+ filename,'wb')       # creates the file where the uploaded file should be stored
             fout.write(x.myfile.file.read())                # writes the uploaded file to the newly created file.
             fout.close()                                    # closes the file, upload complete.
-        doc_print(filedir+"\\"+filename)
+        filename = filedir+"\\"+filename
+        if 'doc' or 'pdf' in filename[-4:]:                
+            doc_print(filename)
+        # else if 'pdf'in filename[-4:]: TODO finish pdf printing
+        #     doc_print(filename,printer='arco')
         raise web.seeother('/upload')
 
 
@@ -41,14 +35,13 @@ if __name__ == "__main__":
 
 
                         
-def doc_print(filename):
+def doc_print(filename,printer='winword'):
     try:
-	   subprocess.run(["winword",filename,"/mFilePrintDefault"],timeout=5)
-    except subprocess.TimeoutExpired as err:
-        return  """
+        subprocess.run([printer,filename,"/mFilePrintDefault"],timeout=30)
+        os.system("taskkill /im "+printer)
+        os.system("del "+filename)
+    except:
+        os.system("del "+filename)   # Clear
+        return """
                 <a href="/upload">Timeout</a>
                 """
-    # os.popen("winword "+filename+" /mFilePrintDefault")
-    # time.sleep(3)
-    # print("slee[")
-    # os.popen("taskkill /im winword.exe")
